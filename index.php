0d4120570f13c20600493d058ca3ca4ad2fca8e9
Guiks <guillaume.delapre@gmail.com>
2015-04-08 14:36:50 +0200

    Création session

--- index.php
+++ index.php
@@ -1,18 +1,32 @@
 <?php 
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+    
     session_start();
 
     require __DIR__ . "/facebook-php-sdk-v4-4.0-dev/autoload.php";
-    echo('test');
+
     use Facebook\FacebookSession;
     use Facebook\FacebookRedirectLoginHelper;
+    use Facebook\FacebookRequest;
+    use Facebook\GraphUser;
     
     const APPID = "764343183684137";
     const APPSECRET = "56ec8f41e39c835873b223320ffdfcae";
     
     FacebookSession::setDefaultApplication(APPID, APPSECRET);
     
-    $helper = new FacebookRedirectLoginHelper('http://localhost');
-    $loginUrl = $helper -> getLoginUrl();
+    $helper = new FacebookRedirectLoginHelper('https://esgi-fb.herokuapp.com/');
+    
+    //SI les variables de sessions existent et que $_SESSION['fb_token'] existe
+    // alors je veux créer mon utilisateur à partir de cette session
+    if( isset($_SESSION) && isset($_SESSION['fb_token']) ){
+        $session = new FacebookSession($_SESSION['fb_token']);
+    }
+    //Sinon j'affiche le lien de connection
+    else {
+        $session = $helper->getSessionFromRedirect();
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -45,8 +59,16 @@
     <div class="fb-like" data-share="true" data-width="450" data-show-faces="true"></div>
 
     <?php
-        $loginUrl = $helper -> getLoginUrl();
-        echo '<a href="' . $loginUrl . '"> Se connecter </a>';
+        if($session){
+            $_SESSION['fb_token'] = (string) $session->getAccessToken();
+            $request_user = new FacebookRequest( $session,"GET","/me");
+            $request_user_executed = $request_user->execute();
+            $user = $request_user_executed->getGraphObject(GraphUser::className());
+            echo "Bonjour ".$user->getName();
+        }else{
+            $loginUrl = $helper->getLoginUrl();
+            echo "<a href='".$loginUrl."'>Se connecter</a>";
+        }
     ?>
   </body>
 </html>
