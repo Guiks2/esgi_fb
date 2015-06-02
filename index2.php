9575bf412aae3f5c51856b15101f25c6f7360b62
ThibaultCrosnier <thibault.crosnier@gmail.com>
2015-06-02 10:51:17 +0200

    Ajout des cadres des photos participantes + Suppression de FitText

--- index2.php
+++ index2.php
@@ -9,28 +9,47 @@
 
 <body>
 	<div id="container-parent">
-    	<div id="container-child-1">
-       		<div id="pic">
-        		<img src="img/swagpizza.png" width="220" height="200"/>
-            </div>
-            <div id="next">
+    	<div id="container-child-1" class="container">
+       		<div id="header">
+        		<img src="img/swagpizza.png" width="180" height="160"/>
+            
             	<p id="accroche">
             		CONCOURS PHOTO SWAG PIZZA
             	</p>
+            </div>
+
+            <div id="next">
             	<p id="description-concours">
                 	Participez en postant une photo de votre plus belle pizza ainsi que sa recette et tentez de gagner votre pizza hebdomadaire pendant 3 mois* !
             	</p>
             </div>
         </div>
         
-        <div id="container-child-2">
+        <div id="container-child-2" class="container">
         	<a href="#" class="red-button">JE PARTICIPE !</a>
+        </div>
+
+        <hr>
+
+         <div id="container-child-3" class="container">
+            <p id="description-vote">
+                 Ou votez pour votre pizza préférée ci-dessous !
+            </p>
+            <div id="photos-candidats">
+                <div class="photo"></div>
+                <div class="photo"></div>
+                <div class="photo"></div>
+                <br/>
+                <div class="photo"></div>
+                <div class="photo"></div>
+                <div class="photo"></div>
+                <br/>
+                <div class="photo"></div>
+                <div class="photo"></div>
+                <div class="photo"></div>
+            </div>
         </div>
     </div>
     <center>* En participant à ce concours, je certifie à l'équipe Swag Pizza d'avoir au moins 18 ans ou d'avoir un accord parental. Offre valable dans la limite de la bonne volonté de l'équipe Swag Pizza.</center>
-	<script>
-		window.fitText(document.getElementById("accroche"), 1.2);
-		window.fitText(document.getElementById("description_concours"), 1.5);
-	</script>
 </body>
 </html>
