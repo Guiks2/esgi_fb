f21f2a3d64c6a41cce443aa2cceb90de422ec2f5
ThibaultCrosnier <thibault.crosnier@gmail.com>
2015-06-16 10:55:34 +0200

    Template de la page concours

--- contest.php
+++ contest.php
@@ -4,10 +4,11 @@
 <meta charset="utf-8">
 <link href="css/main-style.css" rel="stylesheet" type="text/css">
 <title>Concours photo Swag Pizza</title>
+<script src="js/jquery-2.1.4.min.js"></script>
 </head>
 
 <body>
-	<div class="container-parent">
+	<div class="contest-container container-parent">
     	<div id="container-child-1" class="container">
        		<div id="header">
         		<img src="img/swagpizza.png" width="180" height="160"/>
@@ -16,25 +17,21 @@
             		CONCOURS PHOTO SWAG PIZZA
             	</p>
             </div>
-
-            <div id="next">
-            	<p id="description-concours">
-                	Participez en postant une photo de votre plus belle pizza, la plus apétissante ou la plus originale et tentez de gagner votre pizza hebdomadaire pendant 3 mois* !
-            	</p>
-            </div>
         </div>
         
-        <div id="container-child-2" class="container">
-        	<a href="contest.php" class="red-button">JE PARTICIPE !</a>
+        <div id="ask-post" class="container">
+        	<span class="span-ask-post">Je poste ma photo participante</span>
+            <img id="arrow-participate" src="img/arrow.png" />
+            <div id="global-browse-zone">
+                <div id="hide-button"><input type="file" name="browse-image" id="browse-image"></input></div>
+                    <span id="button-browse-image" class="button-upload red-button">Je choisis une image de mon ordinateur...</span><br><span id="ou">OU</span><br>
+                    <span id="button-facebook-image" class="button-upload red-button">Je choisis une image dans mes albums Facebook</span>
+            </div>
         </div>
 
-        <hr>
-
-         <div id="container-child-3" class="container">
-            <p id="description-vote">
-                 Classement actuel
-            </p>
-            <div id="photos-candidats">
+        <hr id="contest-separator">
+        <div id="vote-zone">
+             <div id="photos-candidats">
                 <div class="photo"></div>
                 <div class="photo"></div>
                 <div class="photo"></div>
@@ -47,11 +44,11 @@
                 <div class="photo"></div>
                 <div class="photo"></div>
             </div>
+            <div id="load-more">Charger plus</div>
         </div>
     </div>
     <center>* En participant à ce concours, je certifie à l'équipe Swag Pizza d'avoir au moins 18 ans ou d'avoir un accord parental. Offre valable dans la limite de la bonne volonté de l'équipe Swag Pizza.</center>
 
-    <script src="js/jquery-2.1.4.min.js"></script>
     <script src="js/script.js"></script>
 </body>
 </html>
