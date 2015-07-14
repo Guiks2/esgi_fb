ff09c1b1decdda07275f2b24b1f24f96caf4d14b
ThibaultCrosnier <thibault.crosnier@gmail.com>
2015-07-14 21:48:14 +0200

    Check du problème venant de params

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ FacebookSession::setDefaultApplication(APPID, APPSECRET);
 
 $helper = new FacebookRedirectLoginHelper('https://esgi-fb.herokuapp.com/callback.php');
 $params = array('scope' => 'public_profile,read_stream,publish_actions,user_photos,user_status');
-$loginUrl = $helper->getLoginUrl($params);
+$loginUrl = $helper->getLoginUrl();
    
 }
 
