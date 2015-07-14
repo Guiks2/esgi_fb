4c4639243f711e390a145548cf1373c670150d10
ThibaultCrosnier <thibault.crosnier@gmail.com>
2015-07-14 18:53:34 +0200

    Oops

--- callback.php
+++ callback.php
@@ -4,9 +4,7 @@ session_start();
 
 if(isset($_SESSION["fb_token"]) && !empty($_SESSION["fb_token"])) {
     header("Location: https://esgi-fb.herokuapp.com/contest.php");
-    exit();
 } else {
 	header("Location: http://www.google.fr");
-	exit();
 }
 
