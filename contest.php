b1b52872b0f37b1f7816f93cc8bbbbbb322c74d6
ThibaultCrosnier <thibault.crosnier@gmail.com>
2015-07-21 13:25:06 +0200

    Fix it

--- contest.php
+++ contest.php
@@ -13,10 +13,9 @@
     const APPSECRET = "56ec8f41e39c835873b223320ffdfcae";
 
     FacebookSession::setDefaultApplication(APPID, APPSECRET);
-    $user_id_rq = new FacebookRequest($session, 'GET', '/me');
+    $user_id_rq = new FacebookRequest($session, 'GET', '/me?fields=id');
     $user_id_rp = $user_id_rq->execute();
     $user_id = json_decode($user_id_rp->getRawResponse(), true);
-}
 ?>
 
 <!doctype html>
