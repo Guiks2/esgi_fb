a7bd21a50f4bad40542f42c16e6218e92db1790d
ThibaultCrosnier <thibault.crosnier@gmail.com>
2015-07-22 13:19:04 +0200

    Mise en forme du bouton d'annulation

--- contest.php
+++ contest.php
@@ -80,7 +80,7 @@
                         <div class="uploaded-photo" style="background: url('.$res[3].'); background-size: cover; background-position: center center;></div>"
                         <div id="upload-infos">
                             <span class="your-pic">Votre photo participante</span>
-                            <span class="delete-pic">Annuler la participation</span>
+                            <span class="delete-pic red-button">Enlever cette photo du concours</span>
                         </div>
                     </div>';
         }
