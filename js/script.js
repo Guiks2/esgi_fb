ea69c5e9d5994bbc5756a080ba2bf419623633b2
ThibaultCrosnier <thibault.crosnier@gmail.com>
2015-07-23 02:03:36 +0200

    Appel du script likeManager version 2

--- js/script.js
+++ js/script.js
@@ -90,6 +90,21 @@ $(".delete-pic").on("click", function() {
 
 $(document).on("click", ".photo", function() {
   alert("a");
+  if($(this).attr("data-like") == "true"){
+    console.log("Oui");
+  } else {
+    console.log("Test");
+    var id_pic = $(this).attr("data-id");
+    $.ajax({
+      type:     'POST',
+      url:      "../likeManager.php",
+      data:     {"id_pic": id_pic, "liked": "false"},
+      complete:  function(){
+        console.log("done");
+        $(this).attr("data-like", "true");
+      }
+   });
+  }
 });
 
 /* $("#button-facebook-image").on("click", function() {
